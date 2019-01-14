36f1de06640bf98117887e5630075f878f0aa7e8
Anuj Panchal <anujpan1@gmail.com>
2019-01-14 02:06:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,12 @@ class App < Sinatra::Base
   
   post '/team' do
     @tn = params["team_name"]
+    @coach = params["coach"]
+    @pg = params["point_guard"]
+    @sg = params["shooting_guard"]
+    @sf = params["small_forward"]
+    @pf = params["power_forward"]
+    @center = params["center"]
     erb :team
   end
 end
